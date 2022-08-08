9b750723bdf9c39620389a56636653b79cf86078
Robins-2603 <robinsforwork@gmail.com>
2022-08-09 04:49:11 +0530

    Added: code for valid name in css andin html

--- js/payrollForm.js
+++ js/payrollForm.js
@@ -1,8 +1,18 @@
 // for UC4
 
-const salary = document.querySelector('#salary');
+        const salary = document.querySelector('#salary');
         const output = document.querySelector('.salary-output');
         output.textContent = salary.value;
         salary.addEventListener('input',function() {
             output.textContent = salary.value;
+        });
+
+        // for UC7
+        const text=document.querySelector('#name');
+        const textError=document.querySelector('.text-error');
+        text.addEventListener('input',function(){
+            let nameRegex=RegExp('^[A-Z]{1}[a-z]{2,}$');
+            if (nameRegex.test(text.value))
+            textError.textContent="";
+            else textError.textContent="Name is Incorrect";
         });
