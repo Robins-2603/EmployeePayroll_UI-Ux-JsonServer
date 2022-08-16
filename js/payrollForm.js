654c930b1e2e02f1fde207ce1af767750757ec4a
Robins-2603 <robinsforwork@gmail.com>
2022-08-17 03:47:17 +0530

    Added: UC3 day 44

--- js/payrollForm.js
+++ js/payrollForm.js
@@ -17,36 +17,3 @@
 //             else textError.textContent="Name is Incorrect";
 //         });
 
-        window.addEventListener('DOMContentLoaded', (event) => {
-            salaryOutput();
-            validateName();
-            //validateDate();
-        
-        });
-        
-        function salaryOutput() {
-            const salary = document.querySelector('#salary');
-            const output = document.querySelector('.salary-output');
-            output.textContent = salary.value;
-            salary.addEventListener('input', function() {
-                output.textContent = salary.value;
-        
-            });
-        }
-        
-        function validateName() {
-            const name = document.querySelector('#name');
-            const textError = document.querySelector('.text-error');
-            name.addEventListener('input', function () {
-                if (name.value.length == 0) {
-                    textError.textContent = "";
-                    return;
-                }
-                try {
-                    (new EmployeePayrollData()).name = name.value;
-                    textError.textContent = "";
-                } catch (e) {
-                    textError.textContent = e;
-                }
-            });
-        }
