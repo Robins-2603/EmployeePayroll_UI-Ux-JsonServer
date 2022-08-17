a7c4c591d6780bcd19513f11b404c5f5b4aa8189
Robins-2603 <robinsforwork@gmail.com>
2022-08-17 11:32:08 +0530

    Added: code for uc5 day 45

--- js/newHome.js
+++ js/newHome.js
@@ -28,3 +28,43 @@ window.addEventListener('DOMContentLoaded',(even) => {
         `;
         document.querySelector('#display').innerHTML=innerHtml;
     }
+
+    const createEmployeePayrollJSON=()=>{
+        let empPayrollList =[
+            {
+                _name: "Robins",
+                _gender:"Male",
+                _department: ["ENGINEER","HR"],
+                _salary: "300000",
+                _startDate: "3 Jun 2022",
+                _notes: "",
+                _profilePic: "\assets\profile-images\Ellipse -1.png"
+            },
+            {
+                _name: "Shweta",
+                _gender:"Female",
+                _department: ["FINANCE","ENGINEER","HR"],
+                _salary: "500000",
+                _startDate: "22 May 2019",
+                _notes: "",
+                _profilePic: "\assets\profile-images\Ellipse -2.png"   
+            },
+            {
+                _name: "Nilesh",
+                _gender:"Male",
+                _department: ["ENGINEER","HR"],
+                _salary: "400000",
+                _startDate: "9 Mar 2020",
+                _notes: "",
+                _profilePic: "\assets\profile-images\Ellipse -3.png.png"
+            }
+        ];
+        return empPayrollList;
+    }
+    const getDeptHtml=(deptList) => {
+        let deptHtml='';
+        for(const dept of deptList){
+            deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div`
+        }
+        return deptHtml;
+    }
